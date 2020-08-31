a988489588255643099591db25669f44afe767f9
devcodemani97 <devcodemani97@gmail.com>
2020-08-31 15:30:26 +0530

    sample branch commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 4000;
 app.user('/',(req, res)=>{
     res.send('This is my First Containerized Application');
 })
+app.get('/user',(req, res)=>{
+    res.send('This is my First user Containerized Application');
+})
 app.listen(port, ()=>{
     console.log(`Server is running at ${port}`);
 });
