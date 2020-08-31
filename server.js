20327fa095d0c7f6b051ccfb3a089c2864fafbb2
unknown <devcodemani97@gmail.com>
2020-08-31 13:53:30 +0530

    my second commit

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/',(req, res)=>{
+app.user('/',(req, res)=>{
     res.send('This is my First Containerized Application');
+})
+app.get('/user',(req, res)=>{
+    res.send('This is my First user Containerized Application');
 })
 app.listen(port, ()=>{
     console.log(`Server is running at ${port}`);
